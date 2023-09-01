0e52cc8acacac4d6a012182a888e4a9993f2231b
MicheleGresta <malachia_1997@hotmail.it>
2023-09-01 17:59:54 +0200

    layout statico terminato, da fare carosello

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -6,7 +6,7 @@ export default {
 
 <template>
     <div class="bg-container">
-        <div class="container">
+        <div class="container position-relative">
             <div class="row">
                 <div class="col-4">
                     <img src="../assets/images/logo.png" alt="">
@@ -66,7 +66,7 @@ export default {
                                 <h5>Phone:
                                     <h6>(302) 555-0107</h6>
                                 </h5>
-                                
+
                             </div>
                         </div>
 
@@ -74,12 +74,93 @@ export default {
                 </div>
             </div>
 
+            <div class="row floating-news">
+                <div class="col-1"></div>
+                <div class="col-10">
+                    <div class="bg-row row d-flex align-items-center">
+                        <div class="col-7">
+                            <h3>Subscribe Newsletter</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ipsum quibusdam molestias quo
+                                architecto enim tempore.</p>
+                        </div>
+                        <div class="col-5">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Enter your email" aria-label="Username"
+                                    aria-describedby="basic-addon1">
+                                <span class="input-group-text" id="basic-addon1">
+                                    <img src="../assets/images/icon/send-icon.png" alt="">
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+    <div class="bg-container-2">
+        <div class="container">
+            <div class="row row-footer">
+                <div class="col-4"></div>
+                <div class="col-4 d-flex align-items-center">
+                    <p>Copyright &#xA9; 2022 <span class="color-text">FUTIO</span>. All right reserved.</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
 @use "../variables" as *;
+
+.bg-container-2 {
+    background-color: rgb(27, 33, 45);
+
+    p {
+        color: white;
+        margin: 0;
+        padding: 2rem;
+        .color-text{
+            color: $colore2;
+        }
+    }
+}
+
+.floating-news {
+    position: absolute;
+    top: -25%;
+}
+
+.bg-row {
+    background-color: rgb(48, 65, 95);
+    padding: 4rem 2rem;
+    border-radius: 2rem;
+    color: white;
+}
+
+.form-control {
+    background-color: rgb(67, 83, 116);
+    border: none;
+
+    &::placeholder {
+        color: white;
+    }
+}
+
+.input-group-text {
+    cursor: pointer;
+    background-color: rgb(67, 83, 116);
+    border: none;
+
+    img {
+        background-color: $colore2;
+        border-radius: 50%;
+        padding: .3rem;
+    }
+}
+
+
 
 .bg-container {
     background-color: $card-colore1;
@@ -91,7 +172,7 @@ export default {
         .row {
             .col-4 {
                 color: $colore4;
-                
+
                 .box-socials {
                     flex-basis: 0;
                     flex-grow: 1;
@@ -106,26 +187,30 @@ export default {
 
 
             }
-            .col-8{
-                .row{
-                    .col-3{
-                        h4{
+
+            .col-8 {
+                .row {
+                    .col-3 {
+                        h4 {
                             color: $colore7;
                             margin-bottom: 1.7rem;
                         }
-                        .box-links{
-                            color:$colore4;
-                            h5{
-                                &:hover{
+
+                        .box-links {
+                            color: $colore4;
+
+                            h5 {
+                                &:hover {
                                     cursor: pointer;
-                                    color: $colore1;
+                                    color: $colore2;
                                 }
                             }
-                            
+
                         }
-                        .box-contact-links{
-                            cursor: ;
-                            h5{
+
+                        .box-contact-links {
+
+                            h5 {
                                 color: $colore2;
                             }
                         }
@@ -135,5 +220,4 @@ export default {
         }
 
     }
-}
-</style>
+}</style>
